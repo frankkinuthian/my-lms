625a23ad57c29b4e9bbc1f49ddb2bd8625dee7cd
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-28 20:00:22 +0300

    Add Mailgun variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from environs import Env
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +149,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 AUTH_USER_MODEL = 'userauths.User'
+
+MAILGUN_API_KEY = env("MAILGUN_API_KEY")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
